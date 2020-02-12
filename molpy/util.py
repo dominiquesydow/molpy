0b902e40a9a45271ca2d645d6419dbf106e8258c
dominiquesydow <dominique.sydow@posteo.de>
2020-02-12 14:07:16 +0100

    Util: Add docstring to distance

--- molpy/util.py
+++ molpy/util.py
@@ -1,6 +1,22 @@
 import numpy as np
 
 def distance(point1, point2):
+    """
+    Calcuate distance between two points.
+
+    Parameters
+    ----------
+    point1 : array_like
+        The first point.
+    point2 : array_like
+        The second point.
+
+    Returns
+    -------
+    float
+        The distance between point1 and point2.
+
+    """
 
     point1 = np.asarray(point1)
     point2 = np.asarray(point2)

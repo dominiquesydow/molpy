6c2b0c659905a62164cce474cd174923a41522eb
dominiquesydow <dominique.sydow@posteo.de>
2020-02-13 15:59:03 +0100

    Add class Molecule to molpy.py

--- molpy/molpy.py
+++ molpy/molpy.py
@@ -5,6 +5,9 @@ Molecule manipulation package.
 Handles the primary functions
 """
 
+import numpy as np
+from .util import distance
+
 
 def canvas(with_attribution=True):
     """
@@ -32,6 +35,21 @@ def canvas(with_attribution=True):
 def dummy():
     pass
 
+
+class Molecule:
+
+    def __init__(self, symbols, geometry):
+        self.symbols = np.asarray(symbols, dtype=str)
+        self.geometry = np.asarray(geometry, dtype=float)
+
+        if len(self.geometry.shape) !=2:
+            self.geometry = self.geometry.reshape(-1, 3)
+
+        if self.symbols.shape[0] != self.geometry.shape[0]:
+            raise ValueError(f'Symbol and geometry length does not match!')
+
+    def distance(self, index1, index2):
+        return distance(self.geometry[index1], self.geometry[index2]) 
 
 if __name__ == "__main__":
     # Do something if this file is invoked on its own

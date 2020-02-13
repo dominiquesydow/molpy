90f8209a68080db7d619bd5ccc2524d210e53cbb
dominiquesydow <dominique.sydow@posteo.de>
2020-02-13 08:22:46 +0100

    Data: Add function to read given xyz files

--- molpy/data/reader.py
+++ molpy/data/reader.py
@@ -1,8 +1,36 @@
 import os
 
+from molpy import util
+
 dirname = os.path.dirname(os.path.abspath(__file__))
 filename = os.path.join(dirname, 'look_and_say.dat')
 
 with open(filename, 'r') as handle:
     look_and_say = handle.read()
+
+def get_molecule(molecule):
+    """
+    Get molecule symbols and geometry by molecule name.
+
+    Parameters
+    ----------
+    molecule : str
+        Molecule name.
+    
+    Returns
+    -------
+    dict
+        Molecule symbols and geometry.
+    """
+
+    dirname = os.path.dirname(os.path.abspath(__file__))
+    filename = os.path.join(dirname, 'xyz', f'{molecule}.xyz')
+
+    symbols, coords = util.open_xyz(filename)
+
+    mol = {}
+    mol['symbols'] = symbols
+    mol['geometry'] = coords
+
+    return mol
 
